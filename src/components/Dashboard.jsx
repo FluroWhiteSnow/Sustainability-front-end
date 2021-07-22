6ef6872a1f269abbc29032002235a32916ac508f
Ananda <anrodziewiczz@gmail.com>
2021-07-22 18:42:49 +1000

    added api fetch for user_daily in dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Dashboard() {
-  return <div></div>;
+  const [userDalies, setUserDalies] = useState([]);
+
+  const fetchUserDalies = async () => {
+    const response = await fetch("http://127.0.0.1:3000/api/user_daily", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    // setUserDalies(data);
+  };
+
+  useEffect(() => {
+    fetchUserDalies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <ul>
+        {userDalies.map((daily) => (
+          <li key={daily.id}>{daily}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
