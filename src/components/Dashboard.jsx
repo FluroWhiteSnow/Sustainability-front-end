3da02008b0774099bee578130b1710a995f2c007
Ananda <anrodziewiczz@gmail.com>
2021-07-22 20:07:12 +1000

    added all api calls in dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,29 +1,73 @@
+import { doesNotMatch } from "assert";
 import React, { useEffect, useState } from "react";
 import JSONPretty from "react-json-pretty";
 
 export default function Dashboard() {
-  const [userDalies, setUserDalies] = useState([]);
+  const [user, setUser] = useState();
+  const [userDalies, setUserDalies] = useState();
+  const [cupsTotal, setCupstotal] = useState();
+  const [travelTotal, setTraveltotal] = useState();
+  const [userCo2Daily, setUserCo2Daily] = useState();
+  const [userCo2Total, setUserCo2Total] = useState();
 
-  const fetchUserDalies = async () => {
-    const response = await fetch("http://127.0.0.1:3000/api/user_daily", {
+  const fetchData = async () => {
+    const auth = {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
-    });
-    const data = await response.json();
-    // console.log(data);
-    setUserDalies(data);
-    await console.log(userDalies);
+    };
+
+    const getUser = await fetch("http://127.0.0.1:3000/api/user", auth);
+
+    const getTravelTotal = await fetch(
+      "http://127.0.0.1:3000/api/travel_total",
+      auth
+    );
+
+    const getUserDaily = await fetch(
+      "http://127.0.0.1:3000/api/user_daily",
+      auth
+    );
+    const getCupsTotal = await fetch(
+      "http://127.0.0.1:3000/api/cups_total",
+      auth
+    );
+    const getUserCo2Daily = await fetch(
+      "http://127.0.0.1:3000/api/user_co2_daily",
+      auth
+    );
+    const getCo2Total = await fetch(
+      "http://127.0.0.1:3000/api/user_co2_total",
+      auth
+    );
+
+    const userDaily = await getUserDaily.json();
+    const cupsTotal = await getCupsTotal.json();
+    const travelTotal = await getTravelTotal.json();
+    const userCo2Daily = await getUserCo2Daily.json();
+    const co2Total = await getCo2Total.json();
+
+    setCupstotal(cupsTotal);
+    setUserDalies(userDaily);
+    setTraveltotal(travelTotal);
+    setUserCo2Daily(userCo2Daily);
+    setUserCo2Total(co2Total);
+
+    await console.log(userDaily, cupsTotal, travelTotal, userCo2Daily);
   };
 
   useEffect(() => {
-    fetchUserDalies();
+    fetchData();
   }, []);
 
   return (
     <div>
       <h1>Dashboard Page</h1>
+      <JSONPretty data={cupsTotal}></JSONPretty>
+      <JSONPretty data={travelTotal}></JSONPretty>
       <JSONPretty data={userDalies}></JSONPretty>
+      <JSONPretty data={userCo2Daily}></JSONPretty>
+      <JSONPretty data={userCo2Total}></JSONPretty>
     </div>
   );
 }
