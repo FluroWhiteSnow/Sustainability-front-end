b337c36ef415fec51fe8406979e1ca182bb71f98
Ananda <anrodziewiczz@gmail.com>
2021-07-23 16:37:42 +1000

    added basic styling to sign in page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,30 @@
 import React, { useState } from "react";
 import { useAuth } from "./contexts/AuthProvider";
 import { Link } from "react-router-dom";
+import "../stylesheets/App.css";
+import { Grid, InputAdornment } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import {
+  PermIdentityOutlined,
+  LockOpen,
+  MailOutline,
+  ArrowRightTwoTone,
+} from "@material-ui/icons";
+
+const theme = createTheme({
+  typography: {
+    SignUpLink: {
+      fontSize: 2,
+    },
+  },
+});
 
 export default function Login({ history }) {
   const [errorMessage, setErrorMessage] = useState("");
@@ -47,34 +71,118 @@ export default function Login({ history }) {
   return (
     <div>
       <form noValidate onSubmit={submitLogin}>
-        <h1>{errorMessage}</h1>
-        <input
-          type="text"
-          value={loginForm.username}
-          onChange={changeInput}
-          label="username"
-          name="username"
-          placeholder="username"
-        />
-
-        <input
-          type="text"
-          value={loginForm.email}
-          onChange={changeInput}
-          name="email"
-          placeholder="email"
-        />
-
-        <input
-          type="text"
-          value={loginForm.password}
-          onChange={changeInput}
-          name="password"
-          placeholder="password"
-        />
-
-        <button type="submit">Log In</button>
+        <Grid container style={{ minHeight: "100vh" }}>
+          <Grid
+            container
+            item
+            xs={12}
+            sm={6}
+            alignItems="center"
+            direction="column"
+            justifyContent="space-between"
+            style={{ padding: 10 }}
+          >
+            <div />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                maxWidth: 400,
+                minWidth: 300,
+              }}
+            >
+              <Grid container justifyContent="center">
+                <Typography component="h1" variant="h4">
+                  Sign in
+                </Typography>
+              </Grid>
+              <TextField
+                label="Username"
+                margin="normal"
+                value={loginForm.username}
+                onChange={changeInput}
+                name="username"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PermIdentityOutlined />
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+              <TextField
+                label="Email"
+                margin="normal"
+                value={loginForm.email}
+                onChange={changeInput}
+                name="email"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MailOutline />
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+              <TextField
+                label="Password"
+                margin="normal"
+                value={loginForm.password}
+                onChange={changeInput}
+                name="password"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LockOpen />
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+              <div style={{ height: 20 }} />
+              <Button type="submit" color="primary" variant="contained">
+                Log In
+              </Button>
+              <div style={{ height: 20 }} />
+              <ThemeProvider theme={theme}>
+                <Typography variant="SignUpLink" align="right">
+                  Dont have an account? Sign Up
+                </Typography>
+              </ThemeProvider>
+            </div>
+            <div />
+          </Grid>
+        </Grid>
       </form>
     </div>
   );
 }
+
+// <form noValidate onSubmit={submitLogin}>
+//         <h1>{errorMessage}</h1>
+//         <input
+//           type="text"
+//           value={loginForm.username}
+//           onChange={changeInput}
+//           label="username"
+//           name="username"
+//           placeholder="username"
+//         />
+
+//         <input
+//           type="text"
+//           value={loginForm.email}
+//           onChange={changeInput}
+//           name="email"
+//           placeholder="email"
+//         />
+
+//         <input
+//           type="text"
+//           value={loginForm.password}
+//           onChange={changeInput}
+//           name="password"
+//           placeholder="password"
+//         />
+
+//         <button type="submit">Log In</button>
+//       </form>
