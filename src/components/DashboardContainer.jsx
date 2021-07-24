cd3ca7e98b4b18423f6eaf13717fa7a9052fcd7c
Jackie Sun <jackiesun2@gmail.com>
2021-07-24 15:10:41 +1000

    container components created

--- src/components/DashboardContainer.jsx
+++ src/components/DashboardContainer.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
+import MetricCard from "./MetricCard";
 
 const useStyles = makeStyles(() => ({
   paper: {
-    height: 50,
-    width: 100,
-    backgroundColor: "#F3F2C9",
+    height: 300,
+    width: 500,
+    backgroundColor: "#FFFFFF",
     marginTop: 30,
   },
 }));
 
-export default function MetricCard() {
+export default function DashboardContainer() {
   const classes = useStyles();
-  return <Paper className={classes.paper} />;
+  return (
+    <Paper className={classes.paper}>
+      <MetricCard />
+    </Paper>
+  );
 }
