7e0c082d2d2f914f36add74456057b9acdc32b25
Ananda <anrodziewiczz@gmail.com>
2021-07-26 21:41:13 +1000

    starting tooltip for tree

--- src/components/TreeCard.jsx
+++ src/components/TreeCard.jsx
@@ -5,51 +5,50 @@ import bg3 from "./assets/bg-3.PNG";
 import bg4 from "./assets/bg-4.PNG";
 import bg5 from "./assets/bg-5.PNG";
 import bg6 from "./assets/bg-6.PNG";
+import "../stylesheets/App.css";
 
 export default function TreeCard(props) {
-  //   let data = props.co2Totals;
-  //   console.log(data);
-  //   console.log(localStorage);
+  let data =
+    props.co2Totals.coffee_cups_co2_total +
+    props.co2Totals.drive_co2_total +
+    props.co2Totals.reusable_cups_co2_total +
+    props.co2Totals.walk_co2_total;
 
-  let data = 45;
+  console.log(data);
 
-  console.log(localStorage.getItem("username"));
+  //   let data = 45;
 
   if (!(data > 50) && data < 51) {
     return (
-      <div>
-        <img src={bg1} width="400" height="200" />
+      <div className="tree-container">
+        <img src={bg1} minWidth="400" height="200" />
       </div>
     );
   } else if (!(data > 100) && data < 101) {
     return (
       <div>
-        <img src={bg2} width="400" height="200" />
+        <img src={bg2} minWidth="400" height="200" />
       </div>
     );
   } else if (!(data > 150) && data < 151) {
     return (
       <div>
-        <img src={bg3} width="400" height="200" />
+        <img src={bg3} minWidth="400" height="200" />
       </div>
     );
   } else if (!(data > 200) && data < 201) {
     return (
       <div>
-        <img src={bg4} width="400" height="200" />
+        <img src={bg4} minWidth="" height="200" />
       </div>
     );
   } else if (!(data > 250) && data < 251) {
     return (
       <div>
-        <img src={bg5} width="400" height="200" />
+        <img src={bg5} />
       </div>
     );
   } else {
-    return (
-      <div>
-        <img src={bg6} width="400" height="200" />
-      </div>
-    );
+    return <img src={bg6} className="tree-img" />;
   }
 }
